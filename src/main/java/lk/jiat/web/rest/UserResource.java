7ff2203273965f0e18d90c06a8cb6754f954ab6a
chamikathereal <118957556+chamikathereal@users.noreply.github.com>
2025-06-23 18:51:54 +0530

    June 23 Complete Project

--- src/main/java/lk/jiat/web/rest/UserResource.java
+++ src/main/java/lk/jiat/web/rest/UserResource.java
@@ -1,23 +1,28 @@
 package lk.jiat.web.rest;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 @Path("/users")
 public class UserResource {
 
     @GET
-    public List<String> getUser(){
-        return Arrays.asList(new String[]{"Alex","Brain","Chris"});
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getUser(@PathParam("id") Integer id){
+
+        Map<String, String> map = new HashMap<>();
+        map.put("id", String.valueOf(id));
+        map.put("name", "Chamika");
+        map.put("email", "chamika@gmail.com");
+
+        System.out.println(map);
+
+        return Response.ok(map).build();
     }
 
-    @GET
-    @Path("/{id}")
-    public String getUser(@PathParam("id") String id){
-        return "User ID: " + id;
-    }
 }
